88d56983414aa9014e74c35830d7df5955e09c91
Rajender snowflake1 <rajender.etltesting@gmail.com>
2024-06-09 09:46:21 +0000

    updated files version1

--- models/staging/stg_customer.sql
+++ models/staging/stg_customer.sql
@@ -1,12 +1,16 @@
-{{config(
-    materialized="table",
-)}}
+{{config(materialized="table")}}
 
 with source as (
-    select * from {{source('db01','s_emp')}}),
+    select * from {{source('rajender','s_emp')}}),
 renamed as (
-    select id as customer_id,
-    first_name,
-    last_name
+    select concat(id,'0000') as customer_id,
+    substring(name,1,3) as customer_name,
+    concat(loc,'_place') as customer_location,
+    etl_loaded_date
     from source
-)
+),
+emp_new as (
+    select concat(id,'0000') id,name,loc 
+        from {{source('rajender','emp')}})
+select distinct emp_new.id,emp_new.name,emp_new.loc,renamed.etl_loaded_date from renamed 
+join emp_new on (renamed.customer_id=emp_new.id)
