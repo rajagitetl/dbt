26f4780a23216a1b308e343b5ef881b43812d25c
Rajender snowflake12 <rajender.etltesting@gmail.com>
2024-09-11 06:33:27 +0000

    changes

--- models/staging/emp_new.sql
+++ models/staging/emp_new.sql
@@ -1,3 +1,8 @@
-{{config(materialized='table',unique_key='id')}}
+{{config(materialized='incremental',unique_key='id')}}
 
-select id,concat(name,loc) as both_strings from {{(source('sch01','emp'))}}
+select id,concat(name,loc) as both_strings,
+case when loc='Canada' then 'US-dollar'
+when loc='US' then 'US-dollar'
+when loc='India' then 'ind-rupie'
+when loc='Russia' then 'Rupbee-rus'
+else 'others' end as loc from {{(source('sch01','emp'))}}
