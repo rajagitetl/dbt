d33c4af6b4340a61ccb31daff3a2a63f9cb0e866
Rajender snowflake12 <rajender.etltesting@gmail.com>
2024-09-11 06:49:23 +0000

    changes

--- models/staging/emp_incremental.sql
+++ models/staging/emp_incremental.sql
@@ -1,3 +1,3 @@
 {{config(materialized='incremental')}}
 
-select id,name,loc,current_timestamp as login_updated_date from {{(source("sch01","emp"))}}
+select id,name,loc,login_updated_date from {{(source("sch01","emp_new"))}}
